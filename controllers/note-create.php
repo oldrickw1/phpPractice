01f372be5f7b84a426ac59e5fe35ed8ca80c29d8
Oldrick Weenink <o.weenink@hotmail.com>
2023-11-14 15:24:42 +0100

    Added validation to form

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,5 +1,29 @@
 <?php
 
-$heading = "create a note";
+$config = require('config.php');
+$db = new Database($config['database'], password:"1995priko");
+
+
+
+$heading = "Create a note";
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $errors = [];
+
+    if (strlen($_POST["body"]) == 0) { 
+        $errors['body'] = 'A body is required';
+    }
+
+    if (empty($errors)) {
+        $db->query("INSERT INTO notes (body, user_id) VALUES(:body, :user_id)", [
+            "body"=> $_POST["body"],
+            "user_id"=> 6,
+        ]);
+    }
+
+
+}
 
 require 'views/note-create.view.php';
